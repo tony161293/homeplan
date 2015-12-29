2ef2c951cc4e780150f78d24145e1deac51622fd
tony <tony161293@gmail.com>
2015-12-29 22:36:20 +0530

    nwe

--- homeplan/settings.py
+++ homeplan/settings.py
@@ -81,20 +81,20 @@ WSGI_APPLICATION = 'homeplan.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 
-if not 'DATABASE_URL' in os.environ:
+# if not 'DATABASE_URL' in os.environ:
 
-    DEBUG = True
+#     DEBUG = True
 
-    TEMPLATE_DEBUG = True
+#     TEMPLATE_DEBUG = True
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
 
-else:
+# else:
 
     DEBUG = True
 
